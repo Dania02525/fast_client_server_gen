a528a6fa045da0bf4e8851e7f03973e6685fe1f6
Dania02525 <dania02525@gmail.com>
2023-07-07 10:55:10 -0400

    initial commit

---
+++ lib/fast_client_server_gen.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "fast_client_server_gen/version"
+
+module FastClientServerGen
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/fast_client_server_gen_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe FastClientServerGen do
+  it "has a version number" do
+    expect(FastClientServerGen::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
